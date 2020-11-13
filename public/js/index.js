d89d7261437ace50d5b1d22b79dbf162b5e38723
Charles Helliet <charles.helliet@elamp.fr>
2020-11-13 14:48:08 +0100

    mis en place javascript

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+fetch("http://localhost:3000/api/furniture")
+
+/*
+
+.then(function(response){
+    return response.json()
+})
+
+*/
+
+.then(response => response.json())
+
+//.then(data => console.log(data))
+
+.then(function(data){
+    console.log(data)
+    //code à faire ici
+    //faire une boucle sur tous les produits
+    //dans la boucle ajouter des éléments html avec les données dynamiques
+    //innerHTML et les litéraux de gabarit (template literal)
+})
+
+.catch(error => alert("Erreur : " + error));
+
