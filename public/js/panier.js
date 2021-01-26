7b0e57a6b3a0390abef082577754cd0f9d02abc4
Charles Helliet <charles.helliet@elamp.fr>
2021-01-26 16:38:02 +0100

    maj fetch panier

--- public/js/panier.js
+++ public/js/panier.js
@@ -1,11 +1,11 @@
-let basket = JSON.parse(localStorage.getItem('addedProduct'));
-console.log(basket)
+let basket = JSON.parse(localStorage.getItem("addedProduct"));
+console.log(basket);
 
-//récupération des données de l'API via fetch
-
-fetch("http://localhost:3000/api/furniture/" + basket)
-
-  /*
+//boucle qui affiche les id de chaque produit
+for (const product of basket) {
+  console.log(product);
+  fetch("http://localhost:3000/api/furniture/" + product)
+    /*
   
   .then(function(response){
       return response.json()
@@ -13,28 +13,39 @@ fetch("http://localhost:3000/api/furniture/" + basket)
   
   */
 
-  .then(response => response.json())
+    .then((response) => response.json())
 
-  //.then(data => console.log(data))
+    //.then(data => console.log(data))
 
-  .then(function (productList) {
-    console.log(productList)
-    
-    let basket = localStorage.getItem('addedProduct');
-    console.log(basket)
+    .then(function (productList) {
+      console.log(productList);
 
-    let oneMore = document.querySelector("#basket");
-    oneMore.innerHTML += `<tr>
-    <th scope="row" class="border-0">
-      <div class="p-2">
-        <img src="${productList.imageUrl}" alt="${productList.name}" width="70" class="img-fluid rounded shadow-sm">
-        <div class="ml-3 d-inline-block align-middle">
-          <h5 class="mb-0"> <a href="produit.html?id=${productList._id}" class="text-dark d-inline-block align-middle">${productList.name}</a></h5><span class="text-muted font-weight-normal font-italic d-block">Vernis:${productList.varnish}</span>
-        </div>
-      </div>
-    </th>
-    <td class="border-0 align-middle"><strong>${currency(productList.price)}</strong></td>
-    <td class="border-0 align-middle"><strong>3</strong></td>
-    <td class="border-0 align-middle"><a href="" class="text-dark"><i class="fa fa-trash"></i></a></td>
-  </tr>`
-  });
+      let basket = localStorage.getItem("addedProduct");
+      console.log(basket);
+
+      let oneMore = document.querySelector("#basket");
+      oneMore.innerHTML += `<tr>
+   <th scope="row" class="border-0">
+     <div class="p-2">
+       <img src="${productList.imageUrl}" alt="${
+        productList.name
+      }" width="70" class="img-fluid rounded shadow-sm">
+       <div class="ml-3 d-inline-block align-middle">
+         <h5 class="mb-0"> <a href="produit.html?id=${
+           productList._id
+         }" class="text-dark d-inline-block align-middle">${
+        productList.name
+      }</a></h5><span class="text-muted font-weight-normal font-italic d-block">Vernis:${
+        productList.varnish
+      }</span>
+       </div>
+     </div>
+   </th>
+   <td class="border-0 align-middle"><strong>${currency(
+     productList.price
+   )}</strong></td>
+   <td class="border-0 align-middle"><strong>1</strong></td>
+   <td class="border-0 align-middle"><a href="" class="text-dark"><i class="fa fa-trash"></i></a></td>
+ </tr>`;
+    });
+}
